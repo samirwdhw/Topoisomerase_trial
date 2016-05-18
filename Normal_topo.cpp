380b907cba154851ede53106ad2ae86a67c3cecb
samirwdhw <samir.wdhw@gmail.com>
2016-05-18 15:55:20 +0530

    Initial commit

---
+++ Normal_topo.cpp
@@ -0,0 +1,35 @@
+//This is to simulate the working of Topoisomerase enzyme which
+//removes catenations from DNA which is necessary while replication
+//of DNA. 
+
+//Enzymes randomly surround medium of DNA, we pick latice points
+//equal to number of points and check if the catenation exists
+//and if it does whether it can be removed or not
+
+#include<iostream>
+#include<stdlib.h>
+
+using namespace std;
+
+#define N 100	//SSize of latice
+#define N_ENZYMES 1		//No. Of enzymes around the latice
+#define N_CATS	N/10	//No. of catenations in the latice
+
+int track[N];
+
+void fill_cat(){	//To add randomly placed catenations
+
+
+
+}
+
+void work()		//To simulate a single time step 
+
+int main(){
+
+
+
+
+
+	return 0;
+}
